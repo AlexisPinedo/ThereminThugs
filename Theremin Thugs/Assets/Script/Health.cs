d995cc59887aefb36b4c4df6a505cff61affb4cd
mangouyu <sagwa365nam@aol.com>
2019-02-15 22:45:49 -0800

    initial stuff

---
+++ Theremin Thugs/Assets/Script/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health = 3;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        //Losing a life
+        if (player.gameObject.tag == "Enemy")
+        {
+            health--;
+            Debug.Log("Health is now: " + health);
+        }
+    }
+
+    public Collider2D Collider2D { get; set; }
+
+    //called every frame
+    void Update()
+    {
+
+    }
+    
+}
---
+++ Theremin Thugs/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float horizontalMove = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object forward along its x axis 1 unit/second.
+        transform.Translate(Vector3.right * Time.deltaTime);
+
+        // Move the object forward in world space 1 unit/second.
+        transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+        
+    }
+}
