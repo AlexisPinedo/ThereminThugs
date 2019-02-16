6f5c3fefe301cb096f2d3e25ef289a36a1c6ce05
MONKYBUSINES <coolkath23@gmail.com>
2019-02-15 16:50:17 -0800

    Camera script, Ghost spawn script, incomplete fading scripts

---
+++ Theremin Thugs/Assets/Scripts/EnemyScripts/FadeInScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInScript : MonoBehaviour
+{
+    SpriteRenderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color colour = rend.material.color;
+        colour.a = 0f;
+        rend.material.color = colour;
+    }
+
+    IEnumerator FadeIn()
+    {
+        for(float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color colour = rend.material.color;
+            colour.a = f;
+            rend.material.color = colour;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    public void StartFading()
+    {
+        StartCoroutine("FadeIn");
+    }
+}
---
+++ Theremin Thugs/Assets/Scripts/PlayerScripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public Transform player;
+    public float cameraDistance = 50.0f;
+
+    //For determining distance of camera from player
+    void Awake()
+    {
+        GetComponent<UnityEngine.Camera>().orthographicSize = ((Screen.height / 2) / cameraDistance);
+    }
+
+    //For following player
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);   
+    }
+}
