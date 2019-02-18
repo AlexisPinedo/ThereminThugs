5524611e5f7307f4a9627fd1526b5c9454bdca85
Alex Pinedo <pinedoalex825@gmail.com>
2019-02-17 21:19:47 -0800

    Theremin with input

---
+++ Theremin Thugs/Assets/InputReceiver.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputReceiver : MonoBehaviour
+{
+    public delegate void ScrollWheelUpdate(float value);
+    public static event ScrollWheelUpdate ScrollWheelUpdateEvent;
+
+    public delegate void keyButtonUpdate(int value);
+    public static event keyButtonUpdate keyButtonUpdateEvent;
+
+    public delegate void mouseClickUpdate(int value);
+    public static event mouseClickUpdate mouseClickUpdateEvent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseScrollWheel = Input.GetAxis("Mouse ScrollWheel");
+        if (mouseScrollWheel != 0)
+        {
+            if (ScrollWheelUpdateEvent != null)
+            {
+                ScrollWheelUpdateEvent.Invoke(mouseScrollWheel);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            if (ScrollWheelUpdateEvent != null)
+                keyButtonUpdateEvent.Invoke(0);
+        if (Input.GetKeyDown(KeyCode.W))
+            if (ScrollWheelUpdateEvent != null)
+                keyButtonUpdateEvent.Invoke(1);
+        if (Input.GetKeyDown(KeyCode.E))
+            if (ScrollWheelUpdateEvent != null)
+                keyButtonUpdateEvent.Invoke(2);
+        if (Input.GetKeyDown(KeyCode.R))
+            if (ScrollWheelUpdateEvent != null)
+                keyButtonUpdateEvent.Invoke(3);
+
+        if (Input.GetButtonDown("Lclick"))
+            if (mouseClickUpdateEvent != null)
+                mouseClickUpdateEvent.Invoke(0);
+        if (Input.GetButtonDown("Rclick"))
+            if (mouseClickUpdateEvent != null)
+                mouseClickUpdateEvent.Invoke(2);
+
+    }
+}
---
+++ Theremin Thugs/Assets/Theremin.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Theremin : MonoBehaviour
+{
+    float mouseWheelValue;
+    int note;
+    enum musicNotes{
+        lowC, D, E, F, G, A, B, highC
+    }
+    musicNotes aNote;
+
+    enum volume
+    {
+        softest, soft, loud, loudest
+    }
+    volume theCurrentVolumeSetting;
+
+    enum octave
+    {
+        low, normal, high
+    }
+    octave currentOctave = (octave)1;
+
+    public Text noteText;
+    public Text volumeText;
+    public Text octaveText;
+
+    private void OnEnable()
+    {
+        InputReceiver.ScrollWheelUpdateEvent += receiver_pitchUpdateEvent;
+        InputReceiver.keyButtonUpdateEvent += receiver_volumeUpdateEvent;
+        InputReceiver.mouseClickUpdateEvent += receiver_octaveUpdateEvent;
+    }
+
+    private void Awake()
+    {
+        aNote = 0;
+    }
+
+    private void receiver_pitchUpdateEvent(float value)
+    {
+        if (value > 0)
+        {
+            aNote++;
+            if (aNote.CompareTo(musicNotes.highC) > 0)
+                aNote = 0;
+        }
+        else if(value < 0)
+        {
+            aNote--;
+            if (aNote.CompareTo(musicNotes.lowC) < 0)
+                aNote = musicNotes.highC;
+        }
+        noteText.text = aNote.ToString();
+    }
+
+    private void receiver_volumeUpdateEvent(int value)
+    {
+        theCurrentVolumeSetting = (volume)value;
+        volumeText.text = theCurrentVolumeSetting.ToString();
+    }
+
+    private void receiver_octaveUpdateEvent(int value)
+    {
+        if(currentOctave.CompareTo((octave)value) > 0)
+        {
+            currentOctave--;
+        }
+        else if (currentOctave.CompareTo((octave)value) < 0)
+        {
+            currentOctave++;
+        }
+        octaveText.text = currentOctave.ToString();
+    }
+}
