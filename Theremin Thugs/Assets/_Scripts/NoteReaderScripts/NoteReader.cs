a8d27cf04a341ca70ee91a4587dd389650569690
MONKYBUSINES <coolkath23@gmail.com>
2019-04-12 18:38:53 -0700

    scripts changes

--- Theremin Thugs/Assets/_Scripts/NoteReaderScripts/NoteReader.cs
+++ Theremin Thugs/Assets/_Scripts/NoteReaderScripts/NoteReader.cs
@@ -1,9 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+public enum Note
+{
+
+}
+public struct NoteData
+{
+    Note note;
+    float pitch;
+    float volume;
+    NoteData(Note n, float p, float v = 1)
+    {
+        note = n;
+        pitch = p;
+        volume = v;
+    }
+}
 
 public abstract class NoteReader : MonoBehaviour
 {
- 
+    public Action<NoteData> OnGetNote = delegate (NoteData data) { };
 
+    public virtual NoteData GetNote(float x, float y)
+    {
+        NoteData data = new NoteData();
+        OnGetNote(data);
+        return data;
+    }
 }
--- Theremin Thugs/Assets/_Scripts/NoteReaderScripts/NoteReaderMouse.cs
+++ Theremin Thugs/Assets/_Scripts/NoteReaderScripts/NoteReaderMouse.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class NoteReaderMouse : NoteReader, IPointerEnterHandler, IPointerExitHandler
 {
@@ -17,28 +18,28 @@ public class NoteReaderMouse : NoteReader, IPointerEnterHandler, IPointerExitHan
     public void OnPointerHover()
     {
         Vector2 mousePos = Input.mousePosition;
-        //Debug.Log(mousePos);
-        int height = Screen.height;
-        int width = Screen.width;
+        float height = Screen.height;
+        float width = Screen.width;
+        //RectTransform rt = (RectTransform)this.transform;
+        RectTransform rt = GetComponent<RectTransform>();
+        Rect rect = rt.rect;
+        //CanvasScaler canvasScaler = GetComponentInParent<CanvasScaler>();
+        //mousePos =
 
-        RectTransform rt = (RectTransform)this.transform;
-        Rect r = RectTransformToScreenSpace(rt);
-        Vector2 normalizedMousePos = new Vector2(mousePos.x / r.width, mousePos.y / r.height);
+        Vector2 relativeMousePos = mousePos - (Vector2)rt.position;
+        Vector2 normalizedMousePos = new Vector2(relativeMousePos.x / rt.rect.width, relativeMousePos.y / rt.rect.height);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("rt Transform: " + rt.transform);
-            Debug.Log("Mouse Position: " + (normalizedMousePos));
+            Debug.Log("M: " + mousePos);
+            Debug.Log("Mr: " + relativeMousePos);
+            //Debug.Log(localPoint);
+            Debug.Log("R: " + rt.position + ", " + rt.rect);
+            Debug.Log("N: " + normalizedMousePos);
+            GetNote(normalizedMousePos.x, normalizedMousePos.y);
         }
     }
 
-    public static Rect RectTransformToScreenSpace(RectTransform transform)
-    {
-        Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
-        float x = transform.position.x + transform.anchoredPosition.x;
-        float y = Screen.height - transform.position.y - transform.anchoredPosition.y;
-        return new Rect(x, y, size.x, size.y);
-    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -51,4 +52,23 @@ public class NoteReaderMouse : NoteReader, IPointerEnterHandler, IPointerExitHan
         //Debug.Log("Exit");
         isHoverOver = false;
     }
+
+
+    //private static bool GetLocalMouse(RectTransform rt, out Vector2 result)
+    //{
+    //    Vector2 mousePosition;
+    //    RectTransformUtility.ScreenPointToLocalPointInRectangle(rt, Input.mousePosition, Camera.main, out mousePosition);
+    //    result.x = Mathf.Clamp(mousePosition.x, rt.rect.min.x, rt.rect.max.x);
+    //    result.y = Mathf.Clamp(mousePosition.y, rt.rect.min.y, rt.rect.max.y);
+    //    //Debug.Log(rt.rect.Contains(mousePosition));
+    //    return rt.rect.Contains(mousePosition);
+    //}
+
+    //public static Rect RectTransformToScreenSpace(RectTransform transform)
+    //{
+    //    Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
+    //    float x = transform.position.x + transform.anchoredPosition.x;
+    //    float y = Screen.height - transform.position.y - transform.anchoredPosition.y;
+    //    return new Rect(x, y, size.x, size.y);
+    //}
 }
